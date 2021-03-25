cab5b853d4920dd1605c99c85f045e8035ab5a6d
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2021-03-25 12:32:09 +0100

    Use gray background for related list.

--- components/RelatedList.js
+++ components/RelatedList.js
@@ -5,7 +5,9 @@ import RelatedListItem from "./RelatedListItem.js";
 export default function RelatedList({ dark, list }) {
   const { t } = useI18n();
   return list.length ? (
-    <div className={dark ? "bg-gray-darker text-gray-lighter" : ""}>
+    <div
+      className={dark ? "bg-gray-darker text-gray-lighter" : "bg-gray-lighter"}
+    >
       <div className="container mx-auto px-8 py-16 max-w-screen-lg">
         <h2 className="text-center mb-16">{t("relatedList.title")}</h2>
         <div className="grid gap-16 lg:grid-cols-2">
