d830532c5f74f3a4072f110a2802f0aff9ec7d54
Elias Englen <elias.englen@geops.de>
2021-09-16 18:01:58 +0200

    add styles

--- components/ContactPerson.js
+++ components/ContactPerson.js
@@ -22,7 +22,7 @@ export default function ContactPerson({ person, subtitle, title }) {
         }`}
       >
         <div>
-          <h2>{title || t("contactPerson.title")}</h2>
+          <h2 className="text-5xl">{title || t("contactPerson.title")}</h2>
           {subtitle ? (
             <div
               className={`${subtitleClassName}`}
