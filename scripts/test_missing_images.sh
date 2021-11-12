5885cc118f0bf02a593bfd79e744321df8b77e93
Olivier Terral <olivier.terral@geops.de>
2021-11-12 15:26:30 +0100

    Invert exit code

--- scripts/test_missing_images.sh
+++ scripts/test_missing_images.sh
@@ -3,8 +3,8 @@
 
 if grep "Could not get image size for"; then
   echo "Problem with images, check the log of the build."
-  exit 0 # terminate and indicate error
+  exit 1 # terminate and indicate error
 else
   echo "No problem with images"
-  exit 1
+  exit 0
 fi
