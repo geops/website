89eed9075a941f68514a57451a5ed02d91201ee1
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2021-11-22 09:17:25 +0100

    Show header image in desktop.

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -12,7 +12,7 @@ export default function PageHeader({ src, srcMobile, title, titleDown, text }) {
           src &&
           srcMobile &&
           titleDown === undefined &&
-          "h-[calc(100vh-8rem)] lg:h-auto"
+          "h-[calc(100vh-8rem)] lg:h-[50vh]"
         }`}
       >
         <div className={`relative ${titleDown ? "h-[50vh]" : "h-full"}`}>
@@ -20,6 +20,7 @@ export default function PageHeader({ src, srcMobile, title, titleDown, text }) {
             <Image
               alt={`${title} Cover`}
               className="object-cover w-full"
+              data-cy="pageHeaderImage"
               layout="fill"
               src={src}
             />
@@ -40,13 +41,15 @@ export default function PageHeader({ src, srcMobile, title, titleDown, text }) {
             titleDown === undefined &&
             src &&
             srcMobile &&
-            "absolute lg:static text-white lg:text-gray-darker text-shadow lg:text-shadow-none"
+            "absolute text-white text-shadow"
           }`}
         >
           <h1
             data-cy="pageMainTitle"
             className={`${
-              titleDown ? "lg:mb-8" : "mb-8"
+              titleDown
+                ? "lg:mb-8 lg:text-gray-darker lg:text-shadow-none"
+                : "mb-8"
             } mt-16 mx-auto max-w-screen-lg`}
           >
             {title}
--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -6,4 +6,11 @@ describe("about", () => {
     // The new page should contain an h1 with "About page"
     cy.get('[data-cy="pageMainTitle"]').should("be.visible");
   });
+
+  it("should show desktop header image on desktop", () => {
+    cy.viewport("macbook-15");
+    cy.visit("about");
+
+    cy.get('[data-cy="pageHeaderImage"]').should("be.visible");
+  });
 });
