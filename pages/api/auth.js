1943bf9ccd211a5d0a181e5bf391681f152ec104
danji90 <daniel.marsh-hunn@geops.de>
2022-02-03 14:22:23 +0100

    chore: make linter happy

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,3 +1,3 @@
 import { createVercelBeginHandler } from "netlify-cms-oauth-provider-node"
 
-module.exports = createVercelBeginHandler({}, { useEnv: true })
+module.exports = createVercelBeginHandler({}, { useEnv: true });
