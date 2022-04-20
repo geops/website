a517cdf9a1f92f6ad221ca55c0d6819801e586e5
Friedjoff Trautwein <friedjoff.trautwein@geops.de>
2022-04-20 12:32:31 +0200

    Update styles for about teaser and carousel.

--- components/AboutTeaser.js
+++ components/AboutTeaser.js
@@ -1,17 +1,19 @@
 import { useI18n } from "../lib/i18n";
-import Teaser from "./Teaser";
+import Button from "./Button";
+
 export default function AboutTeaser() {
   const { t } = useI18n();
   return (
-    <div className="bg-gray-darker">
-      <Teaser
-        containerClassName="bg-white max-w-screen-xl xl:rounded mx-auto relative -top-16 md:-top-24"
-        toHeading={true}
-        title={t("aboutTeaser.title")}
-        text={t("aboutTeaser.text")}
-        action={t("website.more")}
-        href="/about"
-      />
+    <div className="bg-white">
+      <div className="container mx-auto">
+        <section className="bg-white max-w-2xl rounded mx-4 lg:ml-8 relative -top-24 lg:-top-36 lg:-mb-24">
+          <div className="p-4 lg:p-8">
+            <h1 className="text-2xl lg:text-5xl">{t("aboutTeaser.title")}</h1>
+            <p className="my-4 lg:my-8">{t("aboutTeaser.text")}</p>
+            <Button href="/about">{t("website.more")}</Button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
