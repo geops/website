df66dd3aa13046d89bb99c6d192f1bff67b68be7
oterral <oterral@users.noreply.github.com>
2023-04-12 15:54:55 +0200

    Add markdown to abaout page (#630)

--- components/AboutContent.js
+++ components/AboutContent.js
@@ -7,22 +7,32 @@ import { useI18n } from "../lib/i18n";
 import Circle from "./Circle.js";
 import aboutImage from "../public/images/page/about/geops-about-text1.jpg";
 import about2Image from "../public/images/page/about/geops-about-text2.jpg";
+import Markdown from "markdown-to-jsx";
 
-const TechnologyCarousel = dynamic(
-  () => import("./TechnologyCarousel"),
-  { ssr: false }
-);
+const TechnologyCarousel = dynamic(() => import("./TechnologyCarousel"), {
+  ssr: false,
+});
 
 export default function AboutContent({ content, technologies }) {
   const { t } = useI18n();
   return (
     <div className="prose prose-xl mx-auto max-w-screen-lg p-8">
       <h2 id="we">{t("about.we")}</h2>
-      <p>{content.we1}</p>
-      <p>{content.we2}</p>
-      <p>{content.we3}</p>
-      <p>{content.we4}</p>
-      <p>{content.we5}</p>
+      <p>
+        <Markdown>{content.we1}</Markdown>
+      </p>
+      <p>
+        <Markdown>{content.we2}</Markdown>
+      </p>
+      <p>
+        <Markdown>{content.we3}</Markdown>
+      </p>
+      <p>
+        <Markdown>{content.we4}</Markdown>
+      </p>
+      <p>
+        <Markdown>{content.we5}</Markdown>
+      </p>
       <div className="my-32 flex flex-col flex-wrap content-center justify-center space-y-8 lg:flex-row lg:justify-between lg:space-y-0">
         <Circle delay="300">
           <div className="text-6xl">{content.circleCounter.year}</div>
@@ -42,14 +52,24 @@ export default function AboutContent({ content, technologies }) {
         </Circle>
       </div>
       <h2 id="work">{t("about.work")}</h2>
-      <p>{content.work}</p>
+      <p>
+        <Markdown>{content.work}</Markdown>
+      </p>
       <Image alt={t("about.work")} className="py-16" src={aboutImage} />
       <h2 id="tech">{t("about.tech")}</h2>
-      <p>{content.tech}</p>
+      <p>
+        <Markdown>{content.tech}</Markdown>
+      </p>
       <TechnologyCarousel slides={technologies} />
       <h2 id="sustainability">{t("about.sustainability")}</h2>
-      <p>{content.sustainability}</p>
-      <Image alt={t("about.sustainability")} className="py-16" src={about2Image} />
+      <p>
+        <Markdown>{content.sustainability}</Markdown>
+      </p>
+      <Image
+        alt={t("about.sustainability")}
+        className="py-16"
+        src={about2Image}
+      />
     </div>
   );
 }
