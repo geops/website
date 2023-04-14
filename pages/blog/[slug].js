5f9fbc1f6cbc96b45137bf462f86b3ab560a30ad
Olivier Terral <olivier.terral@geops.com>
2023-04-14 08:44:13 +0200

    Add structured data for blog

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -6,6 +6,7 @@ import Share from "../../components/Share.js";
 import getContentList from "../../lib/getContentList";
 import getContentBySlug from "../../lib/getContentBySlug";
 import getRelatedContentList from "../../lib/getRelatedContentList";
+import Head from "next/head.js";
 
 export const config = {
   unstable_excludeFiles: ["./content/**", "./public/images/**"],
@@ -25,6 +26,31 @@ export default function BlogArticle({ item, related, ...props }) {
       slugByLocale={item.slugByLocale}
       shareImg={item.cover}
     >
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "NewsArticle",
+              headline: item.title,
+              image: [item.cover],
+              datePublished: item.created,
+              author: [
+                {
+                  "@type": "Organisation",
+                  name: "geOps",
+                  url: "https://geops.com",
+                },
+                {
+                  "@type": "Person",
+                  name: item.author,
+                },
+              ],
+            }),
+          }}
+        />
+      </Head>
       <div className="mx-auto hidden max-w-screen-lg xl:sticky xl:top-4 xl:block">
         {/* > xl*/}
         <Share />
