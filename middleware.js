91f08e3c379da68bb23530400b21cea572d2f561
Olivier Terral <olivier.terral@geops.com>
2023-07-18 15:38:30 +0200

    Readd weird config

--- middleware.js
+++ middleware.js
@@ -56,8 +56,9 @@ export function middleware(request) {
 export const config = {
   matcher: [
     // Skip all internal paths (_next)
-    "/((?!_next).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|admin|sitemap).*)",
     // Optional: only run on root (/) URL
     // '/'
+    ,
   ],
 };
