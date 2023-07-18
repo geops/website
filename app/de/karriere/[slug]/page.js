ecf0102b02e0706f3dae500485468d3a9c9c163a
Olivier Terral <olivier.terral@geops.com>
2023-07-18 15:59:10 +0200

    Add config

--- app/de/karriere/[slug]/page.js
+++ app/de/karriere/[slug]/page.js
@@ -3,6 +3,10 @@ import getContentList from "../../../../lib/getContentList.js";
 import Slug from "../../../../components/CareerArticle.js";
 import { generateCareerSlugMetadata } from "../../../../lib/getMetadata.js";
 
+export const config = {
+  unstable_excludeFiles: ["./content/**", "./public/images/**"],
+};
+
 export async function generateMetadata({ params: { slug } }) {
   const md = await generateCareerSlugMetadata("de", slug);
   return md;
