7c0d5af6ded652801589857eb338bfcc89d7d7a8
Olivier Terral <olivier.terral@geops.com>
2026-01-21 09:57:38 +0100

    fix: use basic img tag

--- components/CustomerLogoCarousel.js
+++ components/CustomerLogoCarousel.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
 import React, { useLayoutEffect, useState } from "react";
 
@@ -22,13 +21,13 @@ export default function CustomerLogoCarousel({ customers = [] }) {
   return (
     <div className="">
       <CarouselProvider
-        visibleSlides={Math.max(Math.floor(width / 300), 1)}
+        visibleSlides={Math.max(Math.floor(width / 250), 1)}
         interval={3000}
         infinite
         isPlaying
         lockOnWindowScroll
-        naturalSlideWidth={300}
-        naturalSlideHeight={300}
+        naturalSlideWidth={250}
+        naturalSlideHeight={250}
         totalSlides={customers.length}
       >
         <Slider
@@ -38,19 +37,22 @@ export default function CustomerLogoCarousel({ customers = [] }) {
         >
           {customers
             .filter((c) => c.logo)
-            .map((customer) => (
-              <Slide
-                key={customer.name}
-                innerClassName="items-center justify-center flex p-6"
-              >
-                <Image
-                  alt={customer.name}
-                  width="300"
-                  height="300"
-                  src={customer.logo}
-                />
-              </Slide>
-            ))}
+            .map((customer) => {
+              console.log("customer logo:", customer);
+              return (
+                <Slide
+                  key={customer.name}
+                  innerClassName="items-center justify-center flex p-6"
+                >
+                  {/* we do not use th next/image because sizing is peinlich */}
+                  <img
+                    alt={customer.name}
+                    src={customer.logo}
+                    className="max-w-[192px] max-h-[125px]"
+                  />
+                </Slide>
+              );
+            })}
         </Slider>
       </CarouselProvider>
     </div>
