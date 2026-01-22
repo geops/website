8444977b6fc6aac096392a9e704db5c7c966fa12
oterral <oterral@users.noreply.github.com>
2026-01-22 14:26:34 +0100

    Add customer logo carousel to front page. (#1090)
    * Add customer logo carousel to front page.
    
    * fix: use caroussel
    
    * fix: add caroussel customers
    
    * fix: add customers
    
    * fix: add customers
    
    * fix: displa ycaroussel
    
    * fix: improve size!
    
    * fix: increase caroussel image size
    
    * fix: use basic img tag
    
    * fix: add description for logo gield
    
    * fix: add description for logo field
    
    * fix: change translations
    
    * fix: shuffle the logo order
    
    * fix: shuffle the logo order
    
    ---------
    
    Co-authored-by: Friedjoff Trautwein <friedjoff.trautwein@geops.com>

---
+++ components/CustomerLogoCarousel.js
@@ -0,0 +1,66 @@
+import { CarouselProvider, Slider, Slide } from "pure-react-carousel";
+import React, { useLayoutEffect, useState } from "react";
+
+import "pure-react-carousel/dist/react-carousel.es.css";
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
+
+export default function CustomerLogoCarousel({ customers = [] }) {
+  const [width] = useWindowSize();
+  return (
+    <div className="">
+      <CarouselProvider
+        visibleSlides={Math.max(Math.floor(width / 250), 1)}
+        interval={3000}
+        infinite
+        isPlaying
+        lockOnWindowScroll
+        naturalSlideWidth={250}
+        naturalSlideHeight={250}
+        totalSlides={customers.length}
+      >
+        <Slider
+          aria-label="Slider"
+          className="overflow-hidden"
+          classNameAnimation="transition ease-in-out duration-1000 h-32 w-auto"
+        >
+          {customers
+            .filter((c) => c.logo)
+            // Shuffle logos
+            .map((customer) => {
+              customer.sortWeight = Math.random();
+              return customer;
+            })
+            .sort((a, b) => a.sortWeight - b.sortWeight)
+            .map((customer) => {
+              console.log("customer logo:", customer);
+              return (
+                <Slide
+                  key={customer.name}
+                  innerClassName="items-center justify-center flex p-6"
+                >
+                  {/* we do not use th next/image because sizing is peinlich */}
+                  <img
+                    alt={customer.name}
+                    src={customer.logo}
+                    className="max-w-[192px] max-h-[125px]"
+                  />
+                </Slide>
+              );
+            })}
+        </Slider>
+      </CarouselProvider>
+    </div>
+  );
+}
